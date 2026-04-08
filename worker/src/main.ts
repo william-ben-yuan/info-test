10aae2992d1e379bac1218684dfd6c7b38b38dd6
William <bk73000@gmail.com>
2026-04-08 10:31:30 -0300

    chore: added README instructions

--- worker/src/main.ts
+++ worker/src/main.ts
@@ -1,19 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
-import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.RMQ,
-    options: {
-      urls: [
-        `amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_PASS}@${process.env.RABBITMQ_HOST}:5672`,
-      ],
-      queue: 'vehicle_events',
-      queueOptions: { durable: true },
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [
+          `amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_PASS}@${process.env.RABBITMQ_HOST}:5672`,
+        ],
+        queue: 'vehicle_events',
+        queueOptions: { durable: true },
+      },
     },
-  });
+  );
   await app.listen();
 }
 bootstrap();
